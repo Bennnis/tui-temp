3854e45597ff49896fd374a3aa0435e2be302233
deris <305945915@qq.com>
2018-05-10 17:52:55 +0800

    init: template

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" id="logo">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 100px;
+  line-height: 30px;
+}
+
+#app #logo {
+  width: 240px;
+  margin-bottom: 50px;
+}
+</style>
